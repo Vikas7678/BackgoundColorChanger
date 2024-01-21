a23559292b541dd69db07f3bbac6138af4197844
Vikas Kumar <vikaskumar9891452674@gmail.com>
2024-01-22 00:23:19 +0530

    Backgound Color changing app using react. This is my first project using react.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('red');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: color, width: "100vw", height: "100vh", display: "flex"}}>
+      <div className='Buttons'>
+        <button style={{backgroundColor: "red", border: "none", borderRadius: "10px", paddingLeft: "15px", paddingRight: "15px",paddingTop: "5px",paddingBottom: "5px"}} onClick={() => setColor('red')}>Red</button>
+        <button style={{backgroundColor: "green", border: "none", borderRadius: "10px", paddingLeft: "15px", paddingRight: "15px",paddingTop: "5px",paddingBottom: "5px"}} onClick={() => setColor('green')}>Green</button>
+        <button style={{backgroundColor: "white", border: "none", borderRadius: "10px", paddingLeft: "15px", paddingRight: "15px",paddingTop: "5px",paddingBottom: "5px"}} onClick={() => setColor('white')}>White</button>
+        <button style={{backgroundColor: "black", color: "white", border: "none", borderRadius: "10px", paddingLeft: "15px", paddingRight: "15px",paddingTop: "5px",paddingBottom: "5px"}} onClick={() => setColor('black')}>Black</button>
+        <button style={{backgroundColor: "olive", border: "none", borderRadius: "10px", paddingLeft: "15px", paddingRight: "15px",paddingTop: "5px",paddingBottom: "5px"}} onClick={() => setColor('olive')}>Olive</button>
+      </div>
     </div>
   );
 }
